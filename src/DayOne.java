661f530960c883d39a2a7fb5b463500a95399431
kairiburnas <kairi.burnasheva@gmail.com>
2024-11-04 16:07:03 +0200

    Hello

--- src/DayOne.java
+++ src/DayOne.java
@@ -5,5 +5,27 @@ public class DayOne {
 
         System.out.println("Kairi");
 
+        int a = 0;
+        int b = 6;
+
+        if (a > 5) {
+            System.out.println("Pidurda");
+        } else if (a > 3) {
+            System.out.println("Pööra");
+        } else {
+            System.out.println("Hüppa");
+        }
+
+        if ((b < 5) && (a > 0)) {
+            System.out.println("Lenda");
+        } else if ((b < 3) || (a > 0)) {
+            System.out.println("Uju");
+        } else{
+            System.out.println("Seisa");
+        }
+
     }
+
 }
+
+
